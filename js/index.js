d64da5a3deef96ce9a5d010ab8ac131b87eff6cf
Ho-Wan <ho.wan2345@gmail.com>
2018-06-03 18:10:07 +0100

    js updated to check if page is on index. removeHash() added.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 // wait for base elements in DOM ready (does not include load() elements)
 $(document).ready(function () {
+  // removes # when page loads
+  removeHash();
   // wait for images and load functions to complete before using getElement.
   window.onload = function () {
+
     // Smooth scroll using jQuery with offset
     const scroll_speed = 800;
     const target_offset = -70;
     $(".link-smooth").on('click', function (event) {
-      console.log("link clicked.")
-      let dest = $(this).attr("dest");
-      if (dest !== "null") {
-        $('html, body').animate({
-          scrollTop: $('#' + dest).offset().top + target_offset
-        }, scroll_speed);
-      } else {
-        throw "'dest' attribute not set";
+      const my_path = window.location.pathname;
+      const dest = $(this).attr("dest");
+      if (my_path === "/" || my_path === "/index.html") {
+        if (dest !== "null") {
+          $('html, body').animate({
+            scrollTop: $('#' + dest).offset().top + target_offset
+          }, scroll_speed);
+        } else {
+          throw "'dest' attribute not set";
+        };
+        return false;
       };
     });
-    // end smooth scroll
+    // end on click
   };
 });
+
+function removeHash() {
+  history.replaceState(null, null, ' ');
+}
