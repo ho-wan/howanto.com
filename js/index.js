13d8e2738b3f1095b21432359c4fa6ff8da5e07c
Ho-Wan <ho.wan2345@gmail.com>
2018-06-03 17:16:44 +0100

    navbar moved back to import.html. window.onload added to js. 3dca navbar top margin removed.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
-// Smooth scroll using jQuery with offset
-$(document).ready(function(){
-  const scroll_speed = 800;
-  const target_offset = -70;
-  $(".link-smooth").on('click', function(event) {
-    let dest = $(this).attr("dest");
-    if (dest !== "null") {
-      $('html, body').animate({
-        scrollTop: $('#' + dest).offset().top + target_offset
-      }, scroll_speed);
-    } else {
-      throw "'dest' attribute not set";
-    }
-  });
+// wait for base elements in DOM ready (does not include load() elements)
+$(document).ready(function () {
+  // wait for images and load functions to complete before using getElement.
+  window.onload = function () {
+    // Smooth scroll using jQuery with offset
+    const scroll_speed = 800;
+    const target_offset = -70;
+    $(".link-smooth").on('click', function (event) {
+      console.log("link clicked.")
+      let dest = $(this).attr("dest");
+      if (dest !== "null") {
+        $('html, body').animate({
+          scrollTop: $('#' + dest).offset().top + target_offset
+        }, scroll_speed);
+      } else {
+        throw "'dest' attribute not set";
+      };
+    });
+    // end smooth scroll
+  };
 });
 
 // Smooth scroll native
