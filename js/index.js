fb9ca76057d826e3291d304e915dff9f781bc2f9
Ho-Wan <ho.wan2345@gmail.com>
2018-06-04 15:57:17 +0100

    window.onload removed since import removed. favicon added.

--- js/index.js
+++ js/index.js
@@ -2,29 +2,24 @@
 $(document).ready(function () {
   // removes # when page loads
   removeHash();
-  // wait for images and load functions to complete before using getElement.
-  window.onload = function () {
-    removeHash();
-
-    // Smooth scroll using jQuery with offset
-    const scroll_speed = 800;
-    const target_offset = -70;
-    $(".link-smooth").on('click', function (event) {
-      const my_path = window.location.pathname;
-      const dest = $(this).attr("dest");
-      if (my_path === "/" || my_path === "/index.html") {
-        if (dest !== "null") {
-          $('html, body').animate({
-            scrollTop: $('#' + dest).offset().top + target_offset
-          }, scroll_speed);
-        } else {
-          throw "'dest' attribute not set";
-        };
-        return false;
+  // Smooth scroll using jQuery with offset
+  const scroll_speed = 800;
+  const target_offset = -70;
+  $(".link-smooth").on('click', function (event) {
+    const my_path = window.location.pathname;
+    const dest = $(this).attr("dest");
+    if (my_path === "/" || my_path === "/index.html") {
+      if (dest !== "null") {
+        $('html, body').animate({
+          scrollTop: $('#' + dest).offset().top + target_offset
+        }, scroll_speed);
+      } else {
+        throw "'dest' attribute not set";
       };
-    });
-    // end on click
-  };
+      return false;
+    };
+  });
+  // end on click
 });
 
 function removeHash() {
